81319594c68f4b5b863d65b7f781196788af0107
Nesanesis <orionvega2343@gmail.com>
2026-04-29 16:35:55 +0300

    feat: Add DB connect

--- Internal/db/db.go
+++ Internal/db/db.go
@@ -1,1 +1,18 @@
 package db
+
+import (
+	"MobeReviewAPI/Internal/config"
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
+// Connect DB
+func Connect(cfg *config.DBConfig) *sqlx.DB {
+	connStr := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable", cfg.Host, cfg.User, cfg.Pass, cfg.Name, cfg.Port)
+	db, err := sqlx.Connect("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+	return db
+}
