86d5fa1d21198b1865756ec5918c1204ed96d909
Nesanesis <orionvega2343@gmail.com>
2026-05-03 19:35:11 +0300

    feat: Add middlewares

--- Internal/middlewares/AuthMiddleware.go
+++ Internal/middlewares/AuthMiddleware.go
@@ -1,1 +1,35 @@
 package middlewares
+
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type User struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	jwt.RegisteredClaims
+}
+
+func AuthMiddleware(next http.Handler, secret string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenStr := r.Header.Get("Authorization")
+		if tokenStr == "" {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		parseToken := strings.TrimPrefix(tokenStr, "Bearer ")
+		token, err := jwt.ParseWithClaims(parseToken, &User{}, func(token *jwt.Token) (interface{}, error) {
+			return []byte(secret), nil
+		})
+		if err != nil || !token.Valid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		ctx := context.WithValue(r.Context(), "user", token.Claims.(*User))
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
